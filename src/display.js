c5f108bf29130fe61dd8130590c53ec520cc9fe0
Sevenpros <sethbisoso@gmail.com>
2022-07-28 01:19:25 +0200

    Add counters Add items counter Add likes counter Add new like Show likes

--- src/display.js
+++ src/display.js
@@ -1,7 +1,24 @@
 export default class Fetch {
-     static displayMeals = async (category = 'Beef') => {
-       const mealsList = await fetch(`https://themealdb.com/api/json/v1/1/filter.php?c=${category}`);
-       const response = await mealsList.json();
-       return response;
-     }
+  static async displayMeals(category = 'Breakfast') {
+    const mealsList = await fetch(`https://themealdb.com/api/json/v1/1/filter.php?c=${category}`);
+    const response = await mealsList.json();
+    return response;
+  }
+
+  static async addLike(meal) {
+    const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Kw76R2UqPqv86GuooSFn/likes/', {
+      method: 'POST',
+      body: JSON.stringify(meal),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return response;
+  }
+
+  static async getLikes() {
+    const mealLikes = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Kw76R2UqPqv86GuooSFn/likes/');
+    const response = mealLikes.json();
+    return response;
+  }
 }
--- src/index.js
+++ src/index.js
@@ -7,22 +7,38 @@ const mealsRow = document.querySelector('.meals-row');
 // get all categories to add event listeners
 const categories = document.querySelectorAll('.category');
 
+
+const showLikes = async (id) => {
+    let likes = 0;
+    const mealLikes = await Fetch.getLikes();
+    await mealLikes.forEach((meal) => {
+      if (meal.item_id === id) {
+        likes = meal.likes;
+      }
+    });
+    return likes;
+  };
+
 // mapping meal cards on the page
 const mapCard = (meals) => {
   // empty the meals section first
   mealsRow.innerHTML = '';
+
   // loop through all given meals
-  meals.forEach((meal) => {
+  meals.forEach(async (meal) => {
+    const likes = await showLikes(meal.idMeal);
     // creating div element to hold one(1) meal info
+
     const mealCard = document.createElement('div');
     mealCard.classList.add('meal-card');
     mealCard.innerHTML = `
         <div class="meal-img"><img alt="${meal.strMeal}" src="${meal.strMealThumb}"></div>
                 <div class="meal-desc">
                     <span class="meal-name">${meal.strMeal}</span>
-                    <div class="meal-likes">
-                        <span class="like-icon"></span>
-                        <span class="like-number"></span>
+                    <div class="meal-likes" id="${meal.idMeal}">
+                    <span class="like-number">${likes}</span>
+                    <a href="#" class="fa fa-thumbs-up like-icon" id="${meal.idMeal}"></a>
+                        
                     </div>
                 </div>
                 <div class="meal-action">
@@ -39,20 +55,48 @@ const mapCard = (meals) => {
 // default function to display the cards on the page (called when a page loads)
 const displayMeals = async () => {
   const mealsList = await Fetch.displayMeals();
-  const meals = await mealsList.meals;
+  const { meals } = await mealsList;
   mapCard(meals);
 };
 
-// add eventlistnener on categories once a specific category is clicked.
+// add eventlistnener on categories when a specific category is clicked.
 categories.forEach((category) => {
-  category.addEventListener('click', (e) => {
-    const category = e.target.textContent;
-    Fetch.displayMeals(category).then((cat) => {
+  category.addEventListener('click', () => {
+    Fetch.displayMeals(category.textContent).then((cat) => {
       const { meals } = cat;
       mapCard(meals);
     });
   });
 });
 
+const itemsCounter = () => {
+  categories.forEach((category) => {
+    const id = category.textContent;
+    const thisLink = document.querySelector(`#${id}`);
+    Fetch.displayMeals(id).then((cat) => {
+      const { meals } = cat;
+      thisLink.textContent = `(${meals.length})`;
+    });
+  });
+};
+mealsRow.addEventListener('click', (e) => {
+  e.preventDefault();
+  const icon = e.target;
+  const likes = icon.previousElementSibling;
+  if (icon.classList.contains('like-icon')) {
+    const meal = {
+      item_id: icon.id,
+    };
+
+    Fetch.addLike(meal).then((m) => m);
+    showLikes(icon.id).then((like) => {
+      likes.innerHTML = like + 1;
+    });
+  }
+});
+
+
+
 // displaying meals on the page by default
+itemsCounter();
 displayMeals();
