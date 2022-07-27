bf9dbc80da1d865b6821f995d1ba4453bf510924
usbbush <jeanbienvenusb@gmail.com>
2022-07-27 10:39:35 +0200

    Update dom-elements.js file

---
+++ src/modules/dom-elements.js
@@ -0,0 +1,7 @@
+export const foodImageBox = document.querySelector('.food-image');
+export const foodNameBox = document.querySelector('.food-name');
+export const categoryBox = document.querySelector('.category');
+export const areaBox = document.querySelector('.area');
+export const ingredientListBox = document.querySelector('.ingredient-list');
+export const tagListBox = document.querySelector('.tag-list');
+
