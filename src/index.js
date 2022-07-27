c1033d296d6d9885b999354e013feb81d55bc3e9
Sevenpros <sethbisoso@gmail.com>
2022-07-27 12:48:38 +0200

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -8,15 +8,15 @@ const mealsRow = document.querySelector('.meals-row');
 const categories = document.querySelectorAll('.category');
 
 // mapping meal cards on the page
-const mapCard = meals => {
-    //empty the meals section first
-    mealsRow.innerHTML = '';
-    // loop through all given meals
-    meals.forEach(meal => {
-        // creating div element to hold one(1) meal info
-        const mealCard = document.createElement('div');
-        mealCard.classList.add('meal-card');
-        mealCard.innerHTML = `
+const mapCard = (meals) => {
+  // empty the meals section first
+  mealsRow.innerHTML = '';
+  // loop through all given meals
+  meals.forEach((meal) => {
+    // creating div element to hold one(1) meal info
+    const mealCard = document.createElement('div');
+    mealCard.classList.add('meal-card');
+    mealCard.innerHTML = `
         <div class="meal-img"><img alt="${meal.strMeal}" src="${meal.strMealThumb}"></div>
                 <div class="meal-desc">
                     <span class="meal-name">${meal.strMeal}</span>
@@ -31,30 +31,28 @@ const mapCard = meals => {
                 </div>
         `;
 
-        // apend meal info to the parent section(declared above)
-        mealsRow.appendChild(mealCard);
-    });
-
-}
+    // apend meal info to the parent section(declared above)
+    mealsRow.appendChild(mealCard);
+  });
+};
 
 // default function to display the cards on the page (called when a page loads)
 const displayMeals = async () => {
-    const mealsList = await Fetch.displayMeals();
-    const meals = await mealsList.meals;
-    mapCard(meals);
-}
+  const mealsList = await Fetch.displayMeals();
+  const meals = await mealsList.meals;
+  mapCard(meals);
+};
 
 // add eventlistnener on categories once a specific category is clicked.
-categories.forEach(category => {
-    category.addEventListener('click', e => {
-        const category = e.target.textContent;
-        Fetch.displayMeals(category).then(cat =>{
-            const meals = cat.meals;
-            mapCard(meals);
-        })
-    })
-})
+categories.forEach((category) => {
+  category.addEventListener('click', (e) => {
+    const category = e.target.textContent;
+    Fetch.displayMeals(category).then((cat) => {
+      const { meals } = cat;
+      mapCard(meals);
+    });
+  });
+});
 
 // displaying meals on the page by default
 displayMeals();
-
