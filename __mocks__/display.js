721a52ef06d8a1f50db12d1ce2b3593e3f8a8638
Sevenpros <sethbisoso@gmail.com>
2022-07-29 11:59:24 +0200

    Fix linter errors

--- __mocks__/display.js
+++ __mocks__/display.js
@@ -1,35 +1,31 @@
 export default class Fetch {
-    constructor() {
-
-    }
-
-    countItems(){
-        let meals = [
-            {
-                "strMeal": "Nasi lemak",
-                "strMealThumb": "https://www.themealdb.com/images/media/meals/wai9bw1619788844.jpg",
-                "idMeal": "53051"
-            },
-            {
-                "strMeal": "Portuguese fish stew (Caldeirada de peixe)",
-                "strMealThumb": "https://www.themealdb.com/images/media/meals/do7zps1614349775.jpg",
-                "idMeal": "53045"
-            },
-            {
-                "strMeal": "Recheado Masala Fish",
-                "strMealThumb": "https://www.themealdb.com/images/media/meals/uwxusv1487344500.jpg",
-                "idMeal": "52809"
-            },
-            {
-                "strMeal": "Salmon Avocado Salad",
-                "strMealThumb": "https://www.themealdb.com/images/media/meals/1549542994.jpg",
-                "idMeal": "52960"
-            },
-            {
-                "strMeal": "Salmon Prawn Risotto",
-                "strMealThumb": "https://www.themealdb.com/images/media/meals/xxrxux1503070723.jpg",
-                "idMeal": "52823"
-            }]
-            return Promise.resolve(meals);
-    }
+  countItems() {
+    this.meals = [
+      {
+        strMeal: 'Nasi lemak',
+        strMealThumb: 'https://www.themealdb.com/images/media/meals/wai9bw1619788844.jpg',
+        idMeal: '53051',
+      },
+      {
+        strMeal: 'Portuguese fish stew (Caldeirada de peixe)',
+        strMealThumb: 'https://www.themealdb.com/images/media/meals/do7zps1614349775.jpg',
+        idMeal: '53045',
+      },
+      {
+        strMeal: 'Recheado Masala Fish',
+        strMealThumb: 'https://www.themealdb.com/images/media/meals/uwxusv1487344500.jpg',
+        idMeal: '52809',
+      },
+      {
+        strMeal: 'Salmon Avocado Salad',
+        strMealThumb: 'https://www.themealdb.com/images/media/meals/1549542994.jpg',
+        idMeal: '52960',
+      },
+      {
+        strMeal: 'Salmon Prawn Risotto',
+        strMealThumb: 'https://www.themealdb.com/images/media/meals/xxrxux1503070723.jpg',
+        idMeal: '52823',
+      }];
+    return Promise.resolve(this.meals);
+  }
 }
--- src/display.js
+++ src/display.js
@@ -19,6 +19,7 @@ export default class Fetch {
     });
     return res;
   }
+
   async countItems(cat) {
     const allMeals = await fetch(this.itemUrl + cat);
     return allMeals.json();
