a9bc0a7cf8acc77e2d1ce00477cbf310af8aacd2
Sevenpros <sethbisoso@gmail.com>
2022-07-29 11:27:25 +0200

    Add tests for item counters

--- src/display.test.js
+++ src/display.test.js
@@ -2,13 +2,18 @@
  * @jest-environment jsdom
  */
 
-import Fetch from "./display.js";
+import Fetch from "../__mocks__/display.js";
 
 const mealList = new Fetch();
 describe('Count Items ', () => {
     it('Should return Number', async () => {
-       await mealList.countItems('Goat').then(meal => {
-        expect(meal.length).toBe(Number);
+       await mealList.countItems().then(meal => {
+        expect(typeof meal.length).toEqual('number');
        })
     })
+    it('Should return 5', async () => {
+        await mealList.countItems().then(meal => {
+         expect(meal.length).toBe(5);
+        })
+     })
 })
